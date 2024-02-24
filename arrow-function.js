0b3813b7f767919c38ca4de5f0f2a80e396e1f49
Naimur00 <aminaim4u@gmail.com>
2024-02-24 12:51:37 +0600

    Another arrow function

--- arrow-function.js
+++ arrow-function.js
@@ -6,3 +6,23 @@ console.log(arrFunc(1, 2, 3));
 
 const defaultParameter = (par1 = 0, par2 = 5) => par1 + par2;
 console.log(defaultParameter(5));
+
+// push into new array from arrow function
+const myfriends = ['Zubaer', 'Tasfiq', 'Ayesha', 'Mahfuz', 'Tahlil', 'Tamim', 'Adnan', 'Zukta', 'Rokan', 'Sumaiya', 'Nirob', 'Shirin', 'Mahedi', 'Hafsa', 'Joy'];
+
+const evenFriends = [];
+const oddFriends = [];
+
+const oddEvenFriend = (arr) => {
+    for (let friend of arr) {
+        if (friend.length % 2 === 0) {
+            evenFriends.push(friend);
+        } else {
+            oddFriends.push(friend);
+        }
+    }
+}
+
+oddEvenFriend(myfriends);
+console.log(evenFriends);
+console.log(oddFriends);
