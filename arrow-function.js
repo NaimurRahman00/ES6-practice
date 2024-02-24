1eece6747bb4ec228c0fcb19fe3031c4c750662a
Naimur00 <aminaim4u@gmail.com>
2024-02-24 12:15:37 +0600

    default parameter function added

--- arrow-function.js
+++ arrow-function.js
@@ -1,2 +1,8 @@
 const arrFunc = (par1, par2, par3) => par1 + par2 + par3;
-console.log(arrFunc(1,2,3));
+console.log(arrFunc(1, 2, 3));
+
+
+// default parameter
+
+const defaultParameter = (par1 = 0, par2 = 5) => par1 + par2;
+console.log(defaultParameter(5));
